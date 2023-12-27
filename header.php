d7218447c04f2f9bfdb1413e3a44d8ab51d04fe1
Noel Gamaliel <techmbala@gmail.com>
2023-12-27 14:32:38 -0800

    changing the extension for the file index

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    <!-- header -->
+        <div class="boxContainer">
+            <nav class="boxContainer">
+                <a href="/">Logo</a>
+                <ul class="box">
+                    <li><a href="http://">Home</a></li>
+                    <li><a href="http://">Blog</a></li>
+                    <li><a href="http://">About As</a></li>
+                </ul>
+                <a href="#" class="btnToggle">
+                    <div class="ligne"></div>
+                    <div class="ligne"></div>
+                </a>
+            </nav>
+        </div>            
+	<!-- End header -->
+</body>
+</html>
--- index.php
+++ index.php
@@ -11,22 +11,9 @@
 </head>
 <body class="">
     <main class="container">
-        <!-- header -->
-        <div class="boxContainer">
-            <nav class="boxContainer">
-                <a href="/">Logo</a>
-                <ul class="box">
-                    <li><a href="http://">Home</a></li>
-                    <li><a href="http://">Blog</a></li>
-                    <li><a href="http://">About As</a></li>
-                </ul>
-                <a href="#" class="btnToggle">
-                    <div class="ligne"></div>
-                    <div class="ligne"></div>
-                </a>
-            </nav>
-        </div>            
-		<!-- End header -->
+        <?php
+            include 'header.php';
+        ?>
 		<br><br><br>
 		
         <!-- hero section -->
